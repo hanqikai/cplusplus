b14622b08bcfeec067c6488213ab97b48ac341ec
hanqikai <hhanqikai@163.com>
2023-02-25 16:51:40 +0800

    20230225

--- test.cc
+++ test.cc
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <string>
+#include <queue>
 using namespace std;
 int main(int argc, char** argv){
-    vector<int> a{10};
+    queue<int> q;
+    priority_queue<int, vector<int>, greater<int>> qq;
+    qq.push(7);
+    qq.push(8);
+    qq.emplace(9);
+    q.push(7);
+    q.push(8);
+    q.emplace(9);
+    
+    while (q.size()) {
+        cout << q.front() << ' ';
+        q.pop();
+    }
+
+    while (qq.size()) {
+        cout << qq.top() << ' ';
+        qq.pop();
+    }
     return 0;
 }
