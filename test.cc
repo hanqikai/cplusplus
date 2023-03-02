380d888c0c1900065f3a07ed8f7ceb05dfc02037
hanqikai <hhanqikai@163.com>
2023-03-02 21:32:27 +0800

    2023.3.2

--- test.cc
+++ test.cc
@@ -4,38 +4,17 @@
 #include <string>
 #include <queue>
 #include <algorithm>
+#include <set>
+#include <map>
 using namespace std;
-struct A
-{
-    
-    
-    int i;
-    char c1;
-    
-};
-struct B
-{
-    char c1;
-    int i;
-    char c2;
-    double d;
-};
-void fcn3() {
-    size_t v1 = 42;  // 局部变量
-    // f可以改变它所捕获的变量的值
-    auto f = [v1] () mutable { return ++v1; };
-    v1 = 0;
-    auto j = f();  // j为43
-    cout << j << endl;
-}
 int main(int argc, char** argv){
-    //fcn3();
-    vector<int> vi{1, 2, 3, 4, 5, 6, -2, -3, -4, -5, -6};
-    transform(vi.begin(), vi.end(), vi.begin(),
-        [=](int i) -> int
-        { if (i < 0)    return -i; else return i; });
-    for(auto x: vi){
-        cout << x << ' ';
+    
+    map<int, int> m;
+    for(vector<int>::size_type i = 10; i != 0; i--) {
+        m[i] = i;
+    }
+    for (auto x: m){
+        cout << x.first << "  " << x.second << endl;
     }
     return 0;
 }
