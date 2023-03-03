d576ef7d8d1cb8261110b48fc21a4208c9e93879
hanqikai <hhanqikai@163.com>
2023-03-03 11:43:07 +0800

    2023.3.3

--- test.cc
+++ test.cc
@@ -13,8 +13,10 @@ int main(int argc, char** argv){
     for(vector<int>::size_type i = 10; i != 0; i--) {
         m[i] = i;
     }
-    for (auto x: m){
-        cout << x.first << "  " << x.second << endl;
+    for (auto x = m.begin(); x != m.end(); x++){
+        cout << (*x).first << "  " << (*x).second << endl;
     }
+    auto d = m.find(1);
+    cout << d->first << " eee " << d->second;
     return 0;
 }
